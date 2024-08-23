54c01eefffd28a52a9a4f88210c62a34b54b698f
Vineww <vcvicari@gmail.com>
2024-08-23 10:50:49 -0300

    Calcular raiz por metodo de biseccao

---
+++ MetodoBiseccao.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+double calculaFx(double x){
+
+    return x * log(3*x - 1);
+
+}
+
+double achaRz(double a, double b, double erro){
+    
+    double xn, 
+           fa, 
+           fx;
+
+    xn = (a + b) / 2;
+
+    fa = calculaFx(a);
+    fx = calculaFx(xn);
+
+    if (fabs(b - a) < erro){
+        
+        return xn;
+
+    }
+
+    if ((fa < 0 && fx < 0) || (fa > 0 && fx > 0)){
+
+        a = xn;
+
+    }else{
+
+        b = xn;
+
+    }
+
+    return achaRz(a, b, erro); 
+
+}
+
+int main(){
+
+float erro  = 0.01,
+       a    = 0.4000,
+       b    = 1.0000;
+
+float raiz = achaRz(a, b, erro);
+
+cout << fixed << setprecision(5);
+cout << "raiz por | Xn - a |: " << raiz << "\n";
+
+return 0;
+
+}
